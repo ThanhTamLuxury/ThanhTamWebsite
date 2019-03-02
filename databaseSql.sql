b98d98432085988dcf4899795542ed2948acbe80
thanhndse <thanhndse@gmail.com>
2019-03-02 16:38:15 +0700

    update to mysql server update script file

--- databaseSql.sql
+++ databaseSql.sql
@@ -1,98 +1,246 @@
-USE master;
-GO
-ALTER DATABASE thanhtamluxury 
-SET SINGLE_USER 
-WITH ROLLBACK IMMEDIATE;
-GO
-DROP DATABASE thanhtamluxury;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 02, 2019 at 09:29 AM
+-- Server version: 5.5.60-MariaDB
+-- PHP Version: 7.2.15
 
-create database thanhtamluxury collate SQL_Latin1_General_CP1_CI_AS
-go
+CREATE SCHEMA `thanhtamluxury`;
+USE `thanhtamluxury`;
 
-USE thanhtamluxury
-GO
-create table Account
-(
-	username varchar(255) not null
-		constraint PK__Account__F3DBC573690E62AB
-			primary key,
-	password varchar(max) not null
-)
-go
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-create table Appointment
-(
-	id int identity
-		constraint PK__Appointm__3213E83F258C4F80
-			primary key,
-	address nvarchar(max),
-	email varchar(255),
-	name nvarchar(255) not null,
-	note nvarchar(max),
-	phone varchar(50),
-	isNew bit
-)
-go
 
-create table Banner
-(
-	id int identity
-		constraint PK__Banner__3213E83FA25FF6FA
-			primary key,
-	path varchar(max)
-)
-go
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table Location
-(
-	id int identity
-		constraint PK__Location__3213E83FD59DA48E
-			primary key,
-	address nvarchar(max),
-	email varchar(255),
-	phone varchar(50),
-	location ntext
-)
-go
+--
+-- Database: `thanhtamluxury`
+--
 
-create table ServiceItem
-(
-	id int identity
-		constraint PK__ServiceI__3213E83FB06EEEB7
-			primary key,
-	description nvarchar(max),
-	name nvarchar(255) not null,
-	price float,
-	priceDescription nvarchar(max),
-	serviceType varchar(255),
-	shortDescription nvarchar(max),
-	slug varchar(max),
-	type nvarchar(255),
-	isActive bit
-)
-go
+-- --------------------------------------------------------
 
-create table ImageItem
-(
-	id int identity
-		constraint PK__ImageIte__3213E83FA984C23C
-			primary key,
-	path varchar(max),
-	serviceItemId int
-		constraint FKofohj8r2v11kfngwcb2b41bk3
-			references ServiceItem
-)
-go
+--
+-- Table structure for table `Account`
+--
 
-create table PriceDetail
-(
-	id int identity
-		primary key,
-	applyDate date,
-	price float not null,
-	serviceItemId int
-		constraint FK2lmyyvrb5trdhf326ux2scuua
-			references ServiceItem
-)
-go
+CREATE TABLE IF NOT EXISTS `Account` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Appointment`
+--
+
+CREATE TABLE IF NOT EXISTS `Appointment` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `note` text,
+  `phone` varchar(50) NOT NULL,
+  `isNew` bit(1) DEFAULT b'1'
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `Banner`
+--
+
+CREATE TABLE IF NOT EXISTS `Banner` (
+  `id` int(11) NOT NULL,
+  `path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Header`
+--
+
+CREATE TABLE IF NOT EXISTS `Header` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `to` varchar(50) DEFAULT NULL,
+  `exact` bit(1) DEFAULT b'1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ImageItem`
+--
+
+CREATE TABLE IF NOT EXISTS `ImageItem` (
+  `id` int(11) NOT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `serviceItemId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Location`
+--
+
+CREATE TABLE IF NOT EXISTS `Location` (
+  `id` int(11) NOT NULL,
+  `address` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `googleLocation` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PriceDetail`
+--
+
+CREATE TABLE IF NOT EXISTS `PriceDetail` (
+  `id` int(11) NOT NULL,
+  `applyDate` date DEFAULT NULL,
+  `price` float NOT NULL,
+  `serviceItemId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ServiceItem`
+--
+
+CREATE TABLE IF NOT EXISTS `ServiceItem` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `serviceType` varchar(255) DEFAULT NULL,
+  `type` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `mainImage` varchar(255) DEFAULT NULL,
+  `location` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `priceDescription` text CHARACTER SET utf8mb4,
+  `shortDescription` text CHARACTER SET utf8mb4,
+  `description` text CHARACTER SET utf8mb4,
+  `isActive` bit(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Account`
+--
+ALTER TABLE `Account`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `Appointment`
+--
+ALTER TABLE `Appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Banner`
+--
+ALTER TABLE `Banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Header`
+--
+ALTER TABLE `Header`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `ImageItem`
+--
+ALTER TABLE `ImageItem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `Location`
+--
+ALTER TABLE `Location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `PriceDetail`
+--
+ALTER TABLE `PriceDetail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ServiceItem`
+--
+ALTER TABLE `ServiceItem`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Appointment`
+--
+ALTER TABLE `Appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=101;
+--
+-- AUTO_INCREMENT for table `Banner`
+--
+ALTER TABLE `Banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Header`
+--
+ALTER TABLE `Header`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ImageItem`
+--
+ALTER TABLE `ImageItem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Location`
+--
+ALTER TABLE `Location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `PriceDetail`
+--
+ALTER TABLE `PriceDetail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ServiceItem`
+--
+ALTER TABLE `ServiceItem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `ImageItem`
+--
+ALTER TABLE `ImageItem`
+  ADD CONSTRAINT `ImageItem_ibfk_1` FOREIGN KEY (`id`) REFERENCES `ServiceItem` (`id`);
+
+--
+-- Constraints for table `PriceDetail`
+--
+ALTER TABLE `PriceDetail`
+  ADD CONSTRAINT `PriceDetail_ibfk_1` FOREIGN KEY (`id`) REFERENCES `ServiceItem` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
