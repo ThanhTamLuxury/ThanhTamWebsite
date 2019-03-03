d8bdca24124f1014e11c25bfa9da3a044e3d976b
datlp0905 <datlp98@gmail.com>
2019-03-03 16:16:50 +0700

    Done api for get all banner

--- thanhtamluxury/src/main/java/com/thanhtam/thanhtamluxury/api/BannerApi.java
+++ thanhtamluxury/src/main/java/com/thanhtam/thanhtamluxury/api/BannerApi.java
@@ -1,7 +1,23 @@
 package com.thanhtam.thanhtamluxury.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.thanhtam.thanhtamluxury.domain.banner.BannerDto;
+import com.thanhtam.thanhtamluxury.domain.banner.BannerService;
+
 @RestController
+@RequestMapping("api/banner")
 public class BannerApi {
+	@Autowired
+	private BannerService bannerService;
+	
+	@GetMapping
+	public List<BannerDto> getAll() {
+		return bannerService.getAll();
+	}
 }
