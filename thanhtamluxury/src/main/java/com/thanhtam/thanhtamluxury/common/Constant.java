444f5ce637b7213b364af2ef20bc219dee34ca6b
datlp0905 <datlp98@gmail.com>
2019-05-05 17:13:01 +0700

    Add error code for all message in Constants

--- thanhtamluxury/src/main/java/com/thanhtam/thanhtamluxury/common/Constant.java
+++ thanhtamluxury/src/main/java/com/thanhtam/thanhtamluxury/common/Constant.java
@@ -7,17 +7,17 @@ public class Constant {
     // error
     public final String APPOINTMENT_ID_NOT_FOUND = "Cannot find appointment with id: ";
 
-    public final String INVALID_SERVICE_ITEM_TYPE = "Invalid service item type: ";
+    public final String INVALID_SERVICE_ITEM_TYPE = "ISIT | Invalid service item type: ";
 
-    public final String SERVICE_ITEM_ID_NOT_FOUND = "Cannot find service item with id: ";
+    public final String SERVICE_ITEM_ID_NOT_FOUND = "SINF | Cannot find service item with id: ";
 
-    public final String SERVICE_ITEM_IDS_NOT_EXISTED = "Service item ids are not existed: ";
+    public final String SERVICE_ITEM_IDS_NOT_EXISTED = "SINE | Service item ids are not existed: ";
 
-    public final String SERVICE_ITEM_NOT_FOUND = "Cannot find service item with id and slug: ";
+    public final String SERVICE_ITEM_NOT_FOUND = "SINF | Cannot find service item with id and slug: ";
 
-    public final String FILE_UPlOAD_NOT_VALID = "Root name is null or multipart file is not valid";
+    public final String FILE_UPlOAD_NOT_VALID = "FUNV | Root name is null or multipart file is not valid";
 
-    public final String BANNER_ID_NOT_FOUND = "Cannot find banner with id: ";
+    public final String BANNER_ID_NOT_FOUND = "BNF | Cannot find banner with id: ";
 
     public final String USERNAME_NOT_FOUND = "UNF | Cannot find account with username: ";
 
@@ -25,5 +25,5 @@ public class Constant {
 
     public final String NEW_PASSWORD_MUST_BE_DIFFERENT = "NPMD | New password must be different with old password";
 
-    public final String FILE_NAME_NOT_FOUND = "Cannot find file with name ";
+    public final String FILE_NAME_NOT_FOUND = "FNF | Cannot find file with name ";
 }
